791d8bc7f7322bc42638609f78b1a3aebc72a387
trungkien19 <kienhoclaptrinh@gmail.com>
2025-07-07 20:14:36 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsfBJA9CfiWzLVZddrwK8tUqOzDITlJm8",
+  authDomain: "tsa-trungkien.firebaseapp.com",
+  projectId: "tsa-trungkien",
+  storageBucket: "tsa-trungkien.appspot.com",
+  messagingSenderId: "954115122394",
+  appId: "1:954115122394:web:3348a44415ad945bfb31e3"
+};
+
+const app = initializeApp(firebaseConfig);
